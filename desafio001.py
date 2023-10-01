e876b51c4fbf533d2a8771e71159944d6ebc2657
DiegoPSP <diego.paulo2022@gmail.com>
2023-10-01 13:01:36 -0300

    Update do desafio

--- desafio001.py
+++ desafio001.py
@@ -1,10 +1,4 @@
-while True:
-    sexo = str(input("Digite seu sexo com apenas uma letra: "))
-    if sexo.upper() == "M":
-        print("Seu sexo é masculino, você é cabra macho!")
-        exit()
-    elif sexo.upper() == "F":
-        print("Seu sexo é feminino, você é uma princesa!")
-        exit()
-    else:
-        print("Não foi possível reconhecer seu sexo, por favor tente novamente.")
+sexo = str(input("Digite seu sexo com apenas uma letra: "))
+while sexo.upper() != "M" and sexo.upper() != "F":
+    sexo = str(input("Inválido! Tente novamente: "))
+print("Masculino" if sexo.upper() == "M"  else "Feminino!")
