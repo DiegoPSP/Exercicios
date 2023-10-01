b39a13cab300d4f397d0d6e435e443e8d0612107
DiegoPSP <diego.paulo2022@gmail.com>
2023-10-01 19:36:28 -0300

    Corrigi o desafio, estava faltando o contador.

--- desafio002.py
+++ desafio002.py
@@ -1,6 +1,8 @@
 import random
 aleatorio = random.randint(0, 10)
+count = 0
 valor = int(input("Tente acertar o numero! Digite: "))
 while valor != aleatorio:
     valor = int(input("Errado! Tente novamente: "))
-print("Você acertou!", aleatorio)
+    count += 1
+print("Você acertou", count,"com tentativa(s)! O valor era:", aleatorio)
