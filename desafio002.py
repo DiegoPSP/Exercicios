76bb5a195e977b9d8952d38468a04cbb39cd9852
DiegoPSP <diego.paulo2022@gmail.com>
2023-10-01 13:03:51 -0300

    ...

--- desafio002.py
+++
@@ -1,4 +0,0 @@
-sexo = str(input("Digite o seu sexo: "))
-while(sexo.upper() != "M" and sexo.upper() != "F"):
-    sexo = str(input("Invalido!! \nDigite o seu sexo: "))
-print("Masculino" if sexo.upper() == "M" else "Feminino")
