61a3eb1d6a4472e9177b4c6a2bc125dfc40af87a
DiegoPSP <diego.paulo2022@gmail.com>
2023-10-01 13:31:31 -0300

    Segundo desafio

--- desafio002.py
+++ desafio002.py
@@ -1,4 +1,5 @@
 import random
+aleatorio = random.randint(0, 10)
 valor = int(input("Tente acertar o numero! Digite: "))
 while valor != aleatorio:
     valor = int(input("Errado! Tente novamente: "))
